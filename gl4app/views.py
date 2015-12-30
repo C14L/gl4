a8ae570e2f4c60f8dbe235f818ebad93b8aa04b4
Christian Steimel <me@C14L.com>
2015-12-30 17:46:51 +0100

    move template file, minor cosmetic fixes

--- gl4app/views.py
+++ gl4app/views.py
@@ -1,8 +1,8 @@
-from django.conf import settings
-from django.shortcuts import render_to_response  # redirect, get_object_or_404
+from django.shortcuts import render_to_response as rtr
 from django.template import RequestContext
-# from django.views.decorators.http import require_http_methods
 
 
-def home(request, t='home.html', c={'settings': settings}):
-    return render_to_response(t, c, context_instance=RequestContext(request))
+def home(request):
+    tpl = 'gl4app/home.html'
+    ctx = {}
+    return rtr(tpl, ctx, context_instance=RequestContext(request))
