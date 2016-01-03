366f9c685a90e29a8823ce3374af33ab534966c3
Christian Steimel <me@C14L.com>
2016-01-03 19:53:53 +0100

    fix stock and company lists on homepage

--- gl4app/views.py
+++ gl4app/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response as rtr
 from django.template import RequestContext
-
 from companydb.models import Stock, Project
 
 
@@ -13,10 +12,20 @@ def home(request):
                      .exclude(profile__country_name='')\
                      .exclude(profile__is_blocked=True)\
                      .exclude(profile__is_deleted=True)\
-                     .order_by('-last_login')
+                     .order_by('-last_login')[:10]
+
+    stocks = []
+    # NotImplementedError: annotate() + distinct(fields) is not implemented.
+    # stocks = Stock.objects.all_public().order_by('user').distinct('user')\
+    #                                    .order_by('-created')[:10]
+    for item in Stock.objects.all_public()[:500]:
+        if item.user.id not in [x.user.id for x in stocks]:
+            stocks.append(item)
+        if len(stocks) >= 10:
+            break
+
+    projects = Project.objects.all_public()[:10]
 
     tpl = 'gl4app/home.html'
-    ctx = {'stocks': Stock.objects.all_public()[:10],
-           'projects': Project.objects.all_public()[:10],
-           'view_users': view_users[:10], }
+    ctx = {'stocks': stocks, 'projects': projects, 'view_users': view_users}
     return rtr(tpl, ctx, context_instance=RequestContext(request))
