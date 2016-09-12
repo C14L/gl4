01dd82ee500b41e5ff878cb9f9fa92016b82fb54
Christian Steimel <me@C14L.com>
2016-09-12 08:43:35 +0200

    Use ENABLE_PROFILER in profiler middleware

--- gl4app/middleware.py
+++ gl4app/middleware.py
@@ -41,13 +41,13 @@ class ProfilerMiddleware(object):
     profiler = None
 
     def process_view(self, request, callback, callback_args, callback_kwargs):
-        if settings.DEBUG and 'prof' in request.GET:
+        if settings.ENABLE_PROFILER and 'prof' in request.GET:
             self.profiler = cProfile.Profile()
             args = (request,) + callback_args
             return self.profiler.runcall(callback, *args, **callback_kwargs)
 
     def process_response(self, request, response):
-        if settings.DEBUG and 'prof' in request.GET:
+        if settings.ENABLE_PROFILER and 'prof' in request.GET:
             self.profiler.create_stats()
             out = StringIO()
             old_stdout, sys.stdout = sys.stdout, out
